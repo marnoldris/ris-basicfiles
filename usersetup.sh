9ed2d5e5b535fb795049f51a7c735d0a187d67dd
marnoldris <100306063+marnoldris@users.noreply.github.com>
2022-09-30 10:09:59 -0600

    Add files via upload

--- usersetup.sh
+++ usersetup.sh
@@ -7,7 +7,13 @@ USERNAME=$1
 PASSWORD=$2
 GROUP=$3
 
+# if the user exists, delete them and all of their files
+if id "$USERNAME" &>/dev/null; then
+    userdel -rf $USERNAME
+fi
+
 # make the user account
+groupadd $GROUP
 useradd -m -g $GROUP -s $(which zsh) $USERNAME
 
 # set user password
@@ -16,6 +22,7 @@ echo $USERNAME:$PASSWORD | chpasswd
 # make directories for the user
 mkdir -p /home/$USERNAME/.config/qtile
 mkdir -p /home/$USERNAME/.config/picom
+mkdir -p /home/$USERNAME/Documents
 mkdir -p /home/$USERNAME/Pictures
 
 
