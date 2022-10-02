1e55b084d4cd802f842b37977fe0aaeb0538e68e
marnoldris <100306063+marnoldris@users.noreply.github.com>
2022-10-01 23:40:01 -0600

    Add files via upload

--- usersetup.sh
+++ usersetup.sh
@@ -12,26 +12,30 @@ PASSWORD=$2
 GROUP=$3
 
 # if the user exists, delete them and all of their files
+# the &>/dev/null sends the standard output and error to null to suppress the output of the commands
 if id "$USERNAME" &>/dev/null; then
     echo "User name matches existing user; deleting user account to start fresh..."
     userdel -rf $USERNAME &>/dev/null
 fi
 
 # make the user account, setting the default group and shell
-# the &>/dev/null sends the standard output and error to null to suppress the output of the commands
 groupadd $GROUP &>/dev/null
+echo "Creating user $USERNAME..."
 useradd -m -g $GROUP -s $(which zsh) $USERNAME
 
 # set user password
+echo "Setting $USERNAME's password..."
 echo $USERNAME:$PASSWORD | chpasswd
 
 # make directories for the user
+echo "Making directories for $USERNAME..."
 mkdir -p /home/$USERNAME/.config/qtile
 mkdir -p /home/$USERNAME/.config/picom
 mkdir -p /home/$USERNAME/Documents
 mkdir -p /home/$USERNAME/Pictures
 
 # install files
+echo "Installing files..."
 install -o $USERNAME -g $GROUP .zshrc.student /home/$USERNAME/.zshrc
 install -o $USERNAME -g $GROUP .vimrc /home/$USERNAME/.vimrc
 install -o $USERNAME -g $GROUP autostart.sh /home/$USERNAME/.config/qtile/
@@ -40,4 +44,8 @@ install -o $USERNAME -g $GROUP config.py /home/$USERNAME/.config/qtile/
 install -o $USERNAME -g $GROUP kde_wp.jpg /home/$USERNAME/Pictures/
 
 # make sure everything is executable
+echo "Making necessary files executable..."
 chmod +x /home/$USERNAME/.config/qtile/autostart.sh
+
+# finished!
+echo "Done!"
