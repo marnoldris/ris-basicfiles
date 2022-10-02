27d33085f75c063aa0dbfe5cd20730976de4a8a7
marnoldris <100306063+marnoldris@users.noreply.github.com>
2022-10-01 23:56:24 -0600

    Update rootsetup.sh

--- rootsetup.sh
+++ rootsetup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 # change shell
+echo "Changing shell..."
 chsh -s $(which zsh)
 
 # copy in files
+echo "Copying essential files..."
 cp .zshrc.root ~/.zshrc
 cp .vimrc ~/.vimrc
 
 # enable system services
+echo "Enabling extra system services..."
 systemctl enable cronie.service sshd.service reflector.service reflector.timer
 
 # start system services
+echo "Attempting to start reflector.timer..."
 systemctl start reflector.timer
+
+# make usersetup.sh executable
+echo "Making usersetup.sh executable..."
+chmod +x ./usersetup.sh
 
 # install cron job for fn keys
 #echo '@reboot root echo 2 > /sys/module/hid_apple/parameters/fnmode' > /etc/cron.d/fnkeys
+
+# finished!
+echo "Done!"
