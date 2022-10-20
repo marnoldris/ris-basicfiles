4cbd40afda2cae97cc5d5cd315046cd909460ab1
Matthew Arnold <marnold@riverstoneschool.org>
2022-10-19 22:04:44 -0600

    Added for loop to add wallpapers in qtile_usersetup.sh

--- qtile_usersetup.sh
+++ qtile_usersetup.sh
@@ -47,7 +47,12 @@ install -v -o $USERNAME -g $GROUP .vimrc /home/$USERNAME/.vimrc
 install -v -o $USERNAME -g $GROUP autostart.sh /home/$USERNAME/.config/qtile/
 install -v -o $USERNAME -g $GROUP picom.conf /home/$USERNAME/.config/picom/
 install -v -o $USERNAME -g $GROUP config.py /home/$USERNAME/.config/qtile/
-install -v -o $USERNAME -g $GROUP kde_wp.jpg /home/$USERNAME/Pictures/wallpaper/
+#install -v -o $USERNAME -g $GROUP kde_wp.jpg /home/$USERNAME/Pictures/wallpaper/
+#TODO add a for loop that iterates through $(ls path/to/wallpaper) and installs each wallpaper
+for wallpaper in $('ls' ./wallpaper/); do
+    install -v -o $USERNAME -g $GROUP $wallpaper /home/$USERNAME/Pictures/wallpaper/
+done
+
 
 # make sure everything is executable
 echo "Making necessary files executable..."
