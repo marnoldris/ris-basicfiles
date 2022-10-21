83caf7c89f3558b692afcdc0bc171f6ce6adf49a
Matthew Arnold <marnold@riverstoneschool.org>
2022-10-21 08:04:09 -0600

    Added F12 hotkey to launch terminal

--- config.py
+++ config.py
@@ -94,6 +94,7 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([], "F12", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod, "shift"], "Tab", lazy.prev_layout(), desc="Toggle between layouts"),
