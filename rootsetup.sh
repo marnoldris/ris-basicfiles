291df9b978b58ab7613bfe7fd965f834c81e8b21
Matthew Arnold <marnold@riverstoneschool.org>
2022-10-24 15:20:39 -0600

    Added systemd service to change function mode on apple computers

--- rootsetup.sh
+++ rootsetup.sh
@@ -5,30 +5,38 @@ echo "Changing shell..."
 chsh -s $(which zsh)
 
 # copy in files
-echo "Copying essential files..."
+echo -e "\nCopying essential files..."
 cp -v .zshrc.root ~/.zshrc
 cp -v .vimrc ~/.vimrc
+#if is_apple
+if [[ -e /sys/module/hid_apple ]]; then
+    echo -e "\nApple computer detected, adding fnmode service..."
+    cp -v fnmode.service /etc/systemd/system/
+fi
 
 # enable system services
-echo "Enabling extra system services..."
+echo -e "\nEnabling extra system services..."
 systemctl enable cronie.service sshd.service reflector.service reflector.timer
+#if is_apple
+if [[ -e /sys/module/hid_apple ]]; then
+    echo -e "\nEnabling fnmode.service..."
+    systemctl enable fnmode.service
+fi
 
 # start system services
-echo "Attempting to start reflector.timer..."
+echo -e "\nAttempting to start reflector.timer..."
 systemctl start reflector.timer
 
 # make files executable
-echo "Making qtile_usersetup.sh and ./kde_usersetup.sh executable..."
+echo -e "\nMaking qtile_usersetup.sh and ./kde_usersetup.sh executable..."
 chmod +x ./qtile_usersetup.sh ./kde_usersetup.sh
 
 # tweak /etc/pacman.conf
-echo "Tweaking /etc/pacman.conf..."
+echo -e "\nTweaking /etc/pacman.conf..."
 sed -i 's/#Color/Color/' /etc/pacman.conf
 sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
-# install cron job for fn keys
-#echo '@reboot root echo 2 > /sys/module/hid_apple/parameters/fnmode' > /etc/cron.d/fnkeys
 
 # finished!
-echo "Done!"
+echo -e "\nDone!"
