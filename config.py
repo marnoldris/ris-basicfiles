875bcd2fa4e7478397cb85b70ad36de9c648d7f8
Matthew Arnold <marnold@riverstoneschool.org>
2022-11-17 22:24:01 -0700

    Moved max layout to be the second in line

--- config.py
+++ config.py
@@ -156,10 +156,10 @@ for i in groups:
 
 layouts = [
     layout.Columns(border_focus="#aaaa00", border_width=2, margin=7, insert_position=1),
+    layout.Max(),
     layout.MonadWide(ratio=0.7, border_focus="#aaaa00", border_width=2, margin=7),
     layout.MonadTall(ratio=0.6, border_focus="#aaaa00", border_width=2, margin=7),
     #layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2,
-    layout.Max(),
     # Try more layouts by unleashing below layouts.
     #layout.Stack(num_stacks=2),
     #layout.Bsp(),
