f251401ae3f6be20f5bf04203a0d5e7cc5692870
marnoldris <100306063+marnoldris@users.noreply.github.com>
2023-05-23 22:58:27 -0600

    Update spyder-install.sh
    Changed the install script so pip will update

--- spyder-install.sh
+++ spyder-install.sh
@@ -3,9 +3,8 @@
 VENV_DIR=$1
 
 mkdir $VENV_DIR
-python -m venv $VENV_DIR
+python -m venv $VENV_DIR --upgrade-deps
 source $VENV_DIR/bin/activate
 python -m pip install spyder numpy scipy pandas matplotlib sympy cython
-python -m pip install --upgrade $(pip freeze)
 
 cp ~/basicfiles/spyder.ini ~/.config/spyder-py3/config/
