c0ec6a2ff6c1b85b2c70eb134cf59089baea221b
Matthew Arnold <marnold@riverstoneschool.org>
2023-05-23 21:56:17 -0600

    Added the config file emplacement

--- spyder-install.sh
+++ spyder-install.sh
@@ -7,3 +7,5 @@ python -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 python -m pip install spyder numpy scipy pandas matplotlib sympy cython
 python -m pip install --upgrade $(pip freeze)
+
+cp ~/basicfiles/spyder.ini ~/.config/spyder-py3/config/
