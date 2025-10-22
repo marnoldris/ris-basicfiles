7250974ae254283d1fdccafa94eeafd5c08fbd89
Matthew Arnold <mlarnold@pm.me>
2025-10-22 11:57:53 -0600

    Tweaks to sway config and spyder-install.sh

--- spyder-install.sh
+++ spyder-install.sh
@@ -6,6 +6,6 @@ echo "Creating directory $VENV_DIR..."
 mkdir $VENV_DIR
 python -m venv $VENV_DIR --upgrade-deps
 source $VENV_DIR/bin/activate
-python -m pip install spyder numpy scipy pandas matplotlib sympy cython
+python -m pip install spyder==5.5.6 numpy scipy pandas matplotlib sympy cython
 
-cp ~/basicfiles/spyder.ini ~/.config/spyder-py3/config/
+cp ~/ris-basicfiles/spyder.ini ~/.config/spyder-py3/config/
