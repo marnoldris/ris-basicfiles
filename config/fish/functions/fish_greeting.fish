20780f8d2baaae217b2555cfad81ab31669247a7
Matt <rivermatt@pm.me>
2025-10-24 00:06:09 -0600

    Added otf-font-awesome linux symbol to fish_greeting.fish

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -2,7 +2,7 @@ function fish_greeting
 	printf "      /\       \n"
 	printf "     /  \      \n"
 	printf "    /    \     \n"
-	printf "   /      \    \n"
+	printf "   /      \   \n"
 	printf "  /   ,,   \   \n"
 	printf " /   |  |   \  \n"
 	printf "/_-''    ''-_\ \n"
