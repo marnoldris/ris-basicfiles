2a4ee10c70f58952d55a6ae8e544def1e155e30d
Matthew Arnold <marnoldris@pm.me>
2025-11-04 12:17:52 -0700

    Centered arch logo in the A

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,9 +1,11 @@
 function fish_greeting
+	printf "\n"
 	printf "      /\       \n"
 	printf "     /  \      \n"
 	printf "    /    \     \n"
-	printf "   /      \   \n"
+	printf "   /     \    \n"
 	printf "  /   ,,   \   \n"
 	printf " /   |  |   \  \n"
 	printf "/_-''    ''-_\ \n"
+	printf "\n"
 end
