01a84552b6ed8c0bf574d2fb110eefc313786737
Matthew <mlarnold@pm.me>
2025-12-05 12:51:43 -0700

    Added KEY functionality

--- okular_lua.lua
+++ okular_lua.lua
@@ -2,8 +2,6 @@
 
 filename = arg[1]
 
--- print(filename)
-
 --[[
 The (.+) match any number of any characters, the %. escapes the .
 so it can be matched and used as the end point.
@@ -17,10 +15,11 @@ The %. escapes the period and uses it as the start point, and
 the (%w+) matches any number of word characters. $ is the end of line
 --]]
 ext = string.match(filename, "%.(%w+)$")
-print(ext)
 
-if ext == 'tex' then
+if ext == 'tex' and not arg[2] then
 	os.execute('okular ' .. extless .. '.pdf &')
+elseif ext == 'tex' and arg[2] == 'KEY' then
+	os.execute('okular ' .. extless .. '_KEY.pdf &')
 else
 	os.execute('okular ' .. filename .. ' &')
 end
